12bf014d41e093c4a561b60b674180cc0eacd358
odyksa <odyksa@myagent.com.ua>
2019-06-03 10:31:03 +0300

    initial commit

---
+++ Weather-Forecast-v2/precache-manifest.5caaa347cfbe91ecd635d51396070b99.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "68f83094dd0472ed3540",
+    "url": "/static/css/main.9223f7b7.chunk.css"
+  },
+  {
+    "revision": "68f83094dd0472ed3540",
+    "url": "/static/js/main.4219e565.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f6afda1b6071757777dd",
+    "url": "/static/js/2.aed6303d.chunk.js"
+  },
+  {
+    "revision": "340ab0560fb17aaa1b56feff3f479b7e",
+    "url": "/index.html"
+  }
+];
