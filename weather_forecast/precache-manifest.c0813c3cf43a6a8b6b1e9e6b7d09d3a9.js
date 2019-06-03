bbe36cb59fc1f02ce2ac777340541fa9be6b2e7e
odyksa <odyksa@myagent.com.ua>
2019-06-03 18:14:50 +0300

    mod

--- weather_forecast/precache-manifest.c0813c3cf43a6a8b6b1e9e6b7d09d3a9.js
+++ weather_forecast/precache-manifest.c0813c3cf43a6a8b6b1e9e6b7d09d3a9.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "6710f6e61029e4e257a3",
+    "revision": "f2c3b919c8225d3ee1a8",
     "url": "/static/css/main.bb104571.chunk.css"
   },
   {
-    "revision": "6710f6e61029e4e257a3",
-    "url": "/static/js/main.5dd69654.chunk.js"
+    "revision": "f2c3b919c8225d3ee1a8",
+    "url": "/static/js/main.ddafe166.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/static/js/2.a39c88ed.chunk.js"
   },
   {
-    "revision": "3780c8fc331a82cb8f000bc275efb7c5",
+    "revision": "53714b2091a492e251d8dd5cc40688d1",
     "url": "/index.html"
   }
 ];
