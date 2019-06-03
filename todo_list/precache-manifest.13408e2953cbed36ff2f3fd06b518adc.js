7cec49c5fcabb2af580701ada8b6c662a80451ba
odyksa <odyksa@myagent.com.ua>
2019-06-03 14:09:50 +0300

    added todolist

---
+++ todo_list/precache-manifest.13408e2953cbed36ff2f3fd06b518adc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bac10ba63e94f1986b91",
+    "url": "/todo_list/static/css/main.c696ffb2.chunk.css"
+  },
+  {
+    "revision": "bac10ba63e94f1986b91",
+    "url": "/todo_list/static/js/main.bac10ba6.chunk.js"
+  },
+  {
+    "revision": "a1fa768bb65d4795a7a9",
+    "url": "/todo_list/static/js/runtime~main.a1fa768b.js"
+  },
+  {
+    "revision": "e873dbf59f56c7ea9a51",
+    "url": "/todo_list/static/js/2.e873dbf5.chunk.js"
+  },
+  {
+    "revision": "ea2aa8bb3f2e05571adca9ff98eaff9f",
+    "url": "/todo_list/index.html"
+  }
+];
