a2604506b178b8ba73838f3eb168c7ae6908644a
odyksa <odyksa@myagent.com.ua>
2019-06-03 17:33:32 +0300

    added weather forecast

---
+++ weather_forecast/precache-manifest.99168ef4c576fac1af4c19ed3a232482.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3c35a7e9ba7eae9cdc1a",
+    "url": "/static/css/main.bb104571.chunk.css"
+  },
+  {
+    "revision": "3c35a7e9ba7eae9cdc1a",
+    "url": "/static/js/main.3449e8ec.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5815492f6fdc381e7a34",
+    "url": "/static/js/2.a39c88ed.chunk.js"
+  },
+  {
+    "revision": "7729006f051e460c20d8203a5ac407fe",
+    "url": "/index.html"
+  }
+];
