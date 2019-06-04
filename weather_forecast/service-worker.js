5f05a4bdcc65362848eca6e9c9c2a5929bfb37a9
odyksa <odyksa@myagent.com.ua>
2019-06-04 11:10:57 +0300

    +

--- weather_forecast/service-worker.js
+++ weather_forecast/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/1/precache-manifest.af033e79a6cfcbbbf4f8a05622a4087c.js"
+  "/weather_forecast/precache-manifest.8248918d07d337dc85070e0dac4cbebc.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/1/index.html", {
+workbox.routing.registerNavigationRoute("/weather_forecast/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
