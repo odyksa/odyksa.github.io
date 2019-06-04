79f9e551f614adea472a0192744ce07a78390b6f
odyksa <odyksa@myagent.com.ua>
2019-06-04 10:53:50 +0300

    +

--- 1/precache-manifest.710db6b5008312472f1c97155cf85482.js
+++ 1/precache-manifest.710db6b5008312472f1c97155cf85482.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "4b8e6407c4960f747024",
+    "revision": "c8545fef491fa8e4d2f4",
     "url": "/1/static/css/main.bb104571.chunk.css"
   },
   {
-    "revision": "4b8e6407c4960f747024",
-    "url": "/1/static/js/main.b371852f.chunk.js"
+    "revision": "c8545fef491fa8e4d2f4",
+    "url": "/1/static/js/main.fb147c59.chunk.js"
   },
   {
     "revision": "6b896846d5f1277321cc",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/1/static/js/2.a39c88ed.chunk.js"
   },
   {
-    "revision": "7a3829290034511add8305df11c241b7",
+    "revision": "1a7717ecdfa048dfdb3b2198791f68ef",
     "url": "/1/index.html"
   }
 ];
--- 1/service-worker.js
+++ 1/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/1/precache-manifest.2141ed490e4a9866d4ccf6e5bd6ecdbb.js"
+  "/1/precache-manifest.710db6b5008312472f1c97155cf85482.js"
 );
 
 workbox.clientsClaim();
