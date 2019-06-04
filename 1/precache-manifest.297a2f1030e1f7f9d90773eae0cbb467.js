7078fce1b6d9d58322a5de7abdfa006a7e2ef1e4
odyksa <odyksa@myagent.com.ua>
2019-06-04 11:24:45 +0300

    +

---
+++ 1/precache-manifest.297a2f1030e1f7f9d90773eae0cbb467.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0cee8e7fb51698000613",
+    "url": "/weather-forecast/static/css/main.bb104571.chunk.css"
+  },
+  {
+    "revision": "0cee8e7fb51698000613",
+    "url": "/weather-forecast/static/js/main.806cbf60.chunk.js"
+  },
+  {
+    "revision": "c753dce0acc1d73f011b",
+    "url": "/weather-forecast/static/js/runtime~main.ff5e48c9.js"
+  },
+  {
+    "revision": "5815492f6fdc381e7a34",
+    "url": "/weather-forecast/static/js/2.a39c88ed.chunk.js"
+  },
+  {
+    "revision": "a1662b559ef8beaafcd54210ebf7ff9d",
+    "url": "/weather-forecast/index.html"
+  }
+];
--- 1/service-worker.js
+++ 1/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/1/precache-manifest.f9f4eb3e1829c1bd5b416d46c2c46615.js"
+  "/weather-forecast/precache-manifest.297a2f1030e1f7f9d90773eae0cbb467.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/1/index.html", {
+workbox.routing.registerNavigationRoute("/weather-forecast/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
