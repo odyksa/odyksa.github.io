1d3b0fd3d5b9fe6d6000a8488a30ea7d88f2c87d
odyksa <odyksa@myagent.com.ua>
2019-06-25 10:25:03 +0300

    add cookbook

---
+++ cookbook/precache-manifest.8e779c343848f1483ab6b4f9ea672d52.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "882cdf11bed3f487d2fcac7506330e36",
+    "url": "/index.html"
+  },
+  {
+    "revision": "01f8fe693724047da3e4",
+    "url": "/static/css/main.1dc6acbb.chunk.css"
+  },
+  {
+    "revision": "4ff7dfeb7d501ff93ab9",
+    "url": "/static/js/2.3a589d80.chunk.js"
+  },
+  {
+    "revision": "01f8fe693724047da3e4",
+    "url": "/static/js/main.9dc3cdba.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
