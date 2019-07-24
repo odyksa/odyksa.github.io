fba89d07951b7d6d42c582f9dd174b6e39296e2a
odyksa <odyksa@myagent.com.ua>
2019-07-24 08:42:08 +0300

    +

---
+++ blueex/precache-manifest.936591d7decccb2b6a5e12495004f2aa.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5e6ecad8d42292e3e42cb1475ad600f",
+    "url": "/blueex/index.html"
+  },
+  {
+    "revision": "1a45a899909858e2b9df",
+    "url": "/blueex/static/css/2.7959e0da.chunk.css"
+  },
+  {
+    "revision": "e095ff450c636eca4eb2",
+    "url": "/blueex/static/css/main.aef8696e.chunk.css"
+  },
+  {
+    "revision": "1a45a899909858e2b9df",
+    "url": "/blueex/static/js/2.d32cbc1f.chunk.js"
+  },
+  {
+    "revision": "e095ff450c636eca4eb2",
+    "url": "/blueex/static/js/main.f0c797fd.chunk.js"
+  },
+  {
+    "revision": "63c05bb337d1413cea71",
+    "url": "/blueex/static/js/runtime~main.28412182.js"
+  },
+  {
+    "revision": "2c11510c7af0aa860356c4600736cf8f",
+    "url": "/blueex/static/media/action-bg.2c11510c.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/blueex/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/blueex/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/blueex/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/blueex/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/blueex/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "9b40096a9e8e738dc2a75fc3a2f931fa",
+    "url": "/blueex/static/media/intro-bg.9b40096a.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/blueex/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/blueex/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/blueex/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/blueex/static/media/slick.f97e3bbf.svg"
+  }
+]);
