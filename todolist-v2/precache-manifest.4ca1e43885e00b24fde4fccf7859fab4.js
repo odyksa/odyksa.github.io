8e9b5700af14e99e84988f63e9c81113e899f0ae
odyksa <odyksa@myagent.com.ua>
2019-08-12 21:00:27 +0300

    +

---
+++ todolist-v2/precache-manifest.4ca1e43885e00b24fde4fccf7859fab4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b41cd8f475150163a5f684587f88c7b",
+    "url": "/todolist-v2/index.html"
+  },
+  {
+    "revision": "185df356d9ba9e960e4e",
+    "url": "/todolist-v2/static/css/main.90e68ea8.chunk.css"
+  },
+  {
+    "revision": "e8fe4fe64398223ba266",
+    "url": "/todolist-v2/static/js/2.eefbbf3b.chunk.js"
+  },
+  {
+    "revision": "185df356d9ba9e960e4e",
+    "url": "/todolist-v2/static/js/main.187e18d2.chunk.js"
+  },
+  {
+    "revision": "045604b7d162876a446b",
+    "url": "/todolist-v2/static/js/runtime~main.40bf5394.js"
+  }
+]);
